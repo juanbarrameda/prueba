4de4c0dfc82f68fc476fa1c48fbc4f12f06b207c
juanma <juanma@WIN7>
2016-06-01 14:28:10 +0100

    Añadimos atributo nombre a la clase banquero.

--- src/dam_ed04_actividad/Banquero.java
+++ src/dam_ed04_actividad/Banquero.java
@@ -12,6 +12,15 @@ public class Banquero {
     
     public Banquero() {
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
     
     
 }
