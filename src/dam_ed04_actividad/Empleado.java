2884cafb817e4e80315abc9aa4496d2a74dab408
juanma <juanma@WIN7>
2016-06-01 13:47:51 +0100

    Creamos una modificacion y un atributo en la clase empleado

--- src/dam_ed04_actividad/Empleado.java
+++ src/dam_ed04_actividad/Empleado.java
@@ -11,5 +11,44 @@ package dam_ed04_actividad;
  */
 public class Empleado {
     private String nombre;  
-    private Integer numero;
+    private String apellidos;  
+    private String dni;
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, String apellidos, String dni) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    
+    
+    
+    
+    
 }
