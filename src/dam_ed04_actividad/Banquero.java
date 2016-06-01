9d5efba1f781c96deec7c35fc2aee2bef6f6f1d9
juanma <juanma@WIN7>
2016-06-01 14:29:45 +0100

    Añadimos get y set a los miembros de la clase Banquero.

--- src/dam_ed04_actividad/Banquero.java
+++ src/dam_ed04_actividad/Banquero.java
@@ -21,6 +21,22 @@ public class Banquero {
         this.nombre = nombre;
     }
 
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
     
     
 }
